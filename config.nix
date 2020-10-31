46fa446cb709c3ccec7a5f2a6cbb9c70edf99341
Hunter Jones <hjones2199@gmail.com>
2020-10-31 16:07:30 -0500

    Initial Commit

---
+++ config.nix
@@ -0,0 +1,70 @@
+rec {
+  allowUnfree = true;
+
+  devPkgs = pkgs: with pkgs; [
+    # Development Packages
+    racket
+    sbcl
+    rustup
+    clang-tools
+    lldb
+    valgrind
+    universal-ctags
+    meson
+    ninja
+    subversion
+    nodejs
+  ];
+
+  desktopPkgs = pkgs: with pkgs; [
+    # Desktop Packages
+    breeze-icons
+	  emacs
+    vscodium
+    mumble
+    bitwarden
+    etcher
+    element-desktop
+    discord
+    torbrowser
+  ];
+
+  termPkgs = pkgs: with pkgs; [
+    # Terminal Tools
+    htop
+    neofetch
+    postgresql_12
+    w3m
+    tmux
+    whois
+    traceroute
+    nettools
+    speedtest-cli
+    powershell
+    bat
+    exa
+    ripgrep
+    tokei
+    tealdeer
+    du-dust
+    fd
+    sd    
+  ];
+
+  packageOverrides = pkgs: with pkgs; {
+    userPackages = pkgs.buildEnv {
+      name = "user-packages";
+      paths = devPkgs pkgs ++
+              desktopPkgs pkgs ++
+              termPkgs pkgs;
+      pathsToLink = [
+        "/bin"
+        "/share/man"
+        "/share/doc"
+        "/share/applications"
+        "/share/icons"
+      ];
+      extraOutputsToInstall = [ "man" "doc" ];
+    };
+  };
+}
